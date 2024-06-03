0b1a3a900efed33b32a43ff75383f9f71640a119
Даниил Радченко <7nexusdanila@gmail.com>
2024-06-03 22:32:36 +0200

    Moved to eventHandling all events.book

--- progetto.mp.radchanka.daniil/src/application/eventHandling/book/borrowedByUser/BorrowedByUserDomainEventHandler.java
+++ progetto.mp.radchanka.daniil/src/application/eventHandling/book/borrowedByUser/BorrowedByUserDomainEventHandler.java
@@ -1,23 +1,23 @@
-package application.events.book.returnByUser;
+package application.eventHandling.book.borrowedByUser;
 
 import base.log.Logger;
 import base.mediator.ddd.DomainEventNotification;
 import base.mediator.notification.NotificationHandler;
 import domain.book.BookId;
-import domain.book.events.BookReturnedByUserDomainEvent;
+import domain.book.events.BookBorrowedByUserDomainEvent;
 import domain.user.User;
 import domain.user.UserId;
 import domain.user.UserRepository;
 import domain.user.exceptions.UserNotFoundException;
 import domain.user.specifications.UserByIdSpecification;
 
-public class ReturnedByUserDomainEventHandler
+public class BorrowedByUserDomainEventHandler
 		implements
-			NotificationHandler<DomainEventNotification<BookReturnedByUserDomainEvent>> {
+			NotificationHandler<DomainEventNotification<BookBorrowedByUserDomainEvent>> {
 	private final Logger logger;
 	private final UserRepository userRepository;
 
-	public ReturnedByUserDomainEventHandler(Logger logger,
+	public BorrowedByUserDomainEventHandler(Logger logger,
 			UserRepository userRepository) {
 		this.logger = logger;
 		this.userRepository = userRepository;
@@ -25,28 +25,29 @@ public class ReturnedByUserDomainEventHandler
 
 	@Override
 	public void handle(
-			DomainEventNotification<BookReturnedByUserDomainEvent> notification) {
+			DomainEventNotification<BookBorrowedByUserDomainEvent> notification) {
 		try {
 			BookId borrowedBookId = notification
 					.getDomainEvent()
-					.getReturnedBook()
+					.getBorrowedBook()
 					.getId();
 			UserId userId = notification
 					.getDomainEvent()
-					.getUserIdThatHadReturnedTheBook();
+					.getUserIdThatHadBorrowedTheBook();
 			User user = userRepository
 					.getFirst(new UserByIdSpecification(userId))
 					.orElseThrow(
 							() -> new UserNotFoundException(
 									"User was not found with id: "
 											+ userId.getId()));
-			if (user.getBorrowedBookIds().contains(borrowedBookId))
-				user.returnBorrowedBook(borrowedBookId);
+
+			if (!user.getBorrowedBookIds().contains(borrowedBookId))
+				user.addBorrowedBook(borrowedBookId);
 		} catch (Exception exc) {
 			logger
 					.logError(
 							"Could not handle {0}: {1}",
-							"ReturnedByUserDomainEvent",
+							"BorrowedByUserDomainEvent",
 							exc.getMessage());
 		}
 	}
--- progetto.mp.radchanka.daniil/src/application/eventHandling/book/returnByUser/ReturnedByUserDomainEventHandler.java
+++ progetto.mp.radchanka.daniil/src/application/eventHandling/book/returnByUser/ReturnedByUserDomainEventHandler.java
@@ -1,23 +1,23 @@
-package application.events.book.borrowedByUser;
+package application.eventHandling.book.returnByUser;
 
 import base.log.Logger;
 import base.mediator.ddd.DomainEventNotification;
 import base.mediator.notification.NotificationHandler;
 import domain.book.BookId;
-import domain.book.events.BookBorrowedByUserDomainEvent;
+import domain.book.events.BookReturnedByUserDomainEvent;
 import domain.user.User;
 import domain.user.UserId;
 import domain.user.UserRepository;
 import domain.user.exceptions.UserNotFoundException;
 import domain.user.specifications.UserByIdSpecification;
 
-public class BorrowedByUserDomainEventHandler
+public class ReturnedByUserDomainEventHandler
 		implements
-			NotificationHandler<DomainEventNotification<BookBorrowedByUserDomainEvent>> {
+			NotificationHandler<DomainEventNotification<BookReturnedByUserDomainEvent>> {
 	private final Logger logger;
 	private final UserRepository userRepository;
 
-	public BorrowedByUserDomainEventHandler(Logger logger,
+	public ReturnedByUserDomainEventHandler(Logger logger,
 			UserRepository userRepository) {
 		this.logger = logger;
 		this.userRepository = userRepository;
@@ -25,29 +25,28 @@ public class BorrowedByUserDomainEventHandler
 
 	@Override
 	public void handle(
-			DomainEventNotification<BookBorrowedByUserDomainEvent> notification) {
+			DomainEventNotification<BookReturnedByUserDomainEvent> notification) {
 		try {
 			BookId borrowedBookId = notification
 					.getDomainEvent()
-					.getBorrowedBook()
+					.getReturnedBook()
 					.getId();
 			UserId userId = notification
 					.getDomainEvent()
-					.getUserIdThatHadBorrowedTheBook();
+					.getUserIdThatHadReturnedTheBook();
 			User user = userRepository
 					.getFirst(new UserByIdSpecification(userId))
 					.orElseThrow(
 							() -> new UserNotFoundException(
 									"User was not found with id: "
 											+ userId.getId()));
-
-			if (!user.getBorrowedBookIds().contains(borrowedBookId))
-				user.addBorrowedBook(borrowedBookId);
+			if (user.getBorrowedBookIds().contains(borrowedBookId))
+				user.returnBorrowedBook(borrowedBookId);
 		} catch (Exception exc) {
 			logger
 					.logError(
 							"Could not handle {0}: {1}",
-							"BorrowedByUserDomainEvent",
+							"ReturnedByUserDomainEvent",
 							exc.getMessage());
 		}
 	}
