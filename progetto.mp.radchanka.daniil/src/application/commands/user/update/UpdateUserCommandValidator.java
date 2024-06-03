ad8286cf07cef29adeb10c1ad7a51879668c4798
Даниил Радченко <7nexusdanila@gmail.com>
2024-06-03 20:42:31 +0200

    Update UpdateUserCommandValidator.java

--- progetto.mp.radchanka.daniil/src/application/commands/user/update/UpdateUserCommandValidator.java
+++ progetto.mp.radchanka.daniil/src/application/commands/user/update/UpdateUserCommandValidator.java
@@ -1,15 +1,21 @@
 package application.commands.user.update;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import application.commands.common.data.AddressCommandData;
 import base.result.Error;
 import base.result.ValidationResult;
 import base.utils.Validator;
 
-public class UpdateUserCommandValidator implements Validator<UpdateUserCommand> {
+public class UpdateUserCommandValidator
+		implements
+			Validator<UpdateUserCommand> {
 
 	private final Validator<AddressCommandData> addressValidator;
 
-	public UpdateUserCommandValidator(Validator<AddressCommandData> addressValidator) {
+	public UpdateUserCommandValidator(
+			Validator<AddressCommandData> addressValidator) {
 
 		this.addressValidator = addressValidator;
 	}
@@ -17,10 +23,30 @@ public class UpdateUserCommandValidator implements Validator<UpdateUserCommand> 
 	@Override
 	public ValidationResult validate(UpdateUserCommand request) {
 		ValidationResult validationResult = ValidationResult.create();
-		return request.getNewAddress().map(addressValidator::validate).map(validationResult::unionWith)
+
+		long providedFieldsCount = Stream
+				.of(request.getNewAddress(), request.getNewPhoneNumber())
+				.filter(Optional::isPresent)
+				.count();
+
+		return request
+				.getNewAddress()
+				.map(addressValidator::validate)
+				.map(validationResult::unionWith)
 				.orElse(validationResult)
-				.withErrorIf(() -> request.getNewPhoneNumber().map(String::isBlank).orElse(false),
-						Error.from("Phone number must be not whitespace if provided"));
+				.withErrorIf(
+						() -> providedFieldsCount == 0,
+						Error
+								.from(
+										"At least new phone number or new address must be present"))
+				.withErrorIf(
+						() -> request
+								.getNewPhoneNumber()
+								.map(String::isBlank)
+								.orElse(false),
+						Error
+								.from(
+										"Phone number must be not whitespace if provided"));
 
 	}
 }
