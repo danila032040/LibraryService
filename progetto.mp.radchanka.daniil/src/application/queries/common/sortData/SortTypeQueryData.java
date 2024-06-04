664f8f60272a13c5300873357fce07e97b0064f7
Даниил Радченко <7nexusdanila@gmail.com>
2024-06-04 23:04:59 +0200

    Refactored BookSortCriteriaBuilderImpl

--- progetto.mp.radchanka.daniil/src/application/queries/common/sortData/SortTypeQueryData.java
+++ progetto.mp.radchanka.daniil/src/application/queries/common/sortData/SortTypeQueryData.java
@@ -1,5 +1,19 @@
 package application.queries.common.sortData;
 
+import base.repository.SortType;
+
 public enum SortTypeQueryData {
-	Ascending, Descending;
+	Ascending {
+		@Override
+		public SortType mapToSortType() {
+			return SortType.Ascending;
+		}
+	},
+	Descending {
+		@Override
+		public SortType mapToSortType() {
+			return SortType.Descending;
+		}
+	};
+	public abstract SortType mapToSortType();
 }
---
+++ progetto.mp.radchanka.daniil/src/base/utils/ComparatorUtils.java
@@ -0,0 +1,19 @@
+package base.utils;
+
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.function.Function;
+
+public final class ComparatorUtils {
+	private ComparatorUtils() {
+
+	}
+
+	public static <T, U extends Comparable<U>> Comparator<T> comparingOptionalField(
+			Function<T, Optional<U>> keyExtractor) {
+		return Comparator
+				.<T, Optional<U>>comparing(
+						keyExtractor,
+						new OptionalComparator<U>());
+	}
+}
