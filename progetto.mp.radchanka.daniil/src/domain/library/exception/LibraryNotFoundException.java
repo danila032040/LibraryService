7a2121e75f786edf2cdf3e5d92d45402699d8395
Даниил Радченко <7nexusdanila@gmail.com>
2024-06-08 23:06:25 +0200

    Removed NotFoundExeptions for Author and Library

--- progetto.mp.radchanka.daniil/src/domain/library/exception/LibraryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package domain.library.exception;
-
-public class LibraryNotFoundException extends Exception {
-    private static final long serialVersionUID = 1L;
-    
-    public LibraryNotFoundException(String message) {
-        super(message);
-    }
-}
